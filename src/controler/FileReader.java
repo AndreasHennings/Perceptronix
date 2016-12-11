66ec336ff86ee0e7d40a4b5ce98923c9a8b51774
aend <a.h.1974@gmx.de>
2016-12-11 22:34:52 +0100

    New FileReader class

---
+++ src/controler/FileReader.java
@@ -0,0 +1,15 @@
+package controler;
+
+import java.util.ArrayList;
+
+/**
+ * Created by aend on 11.12.16.
+ */
+public class FileReader
+{
+    public static ArrayList<String> getAllStrings(String filename)
+    {
+        return null;
+
+    }
+}
