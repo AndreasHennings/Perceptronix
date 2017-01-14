0a7246338e931a834ac73c6ecef57447d65160a2
aend <a.h.1974@gmx.de>
2017-01-14 11:52:02 +0100

    Added GHBridge

--- src/controler/GithubBridge.java
+++ src/controler/GithubBridge.java
@@ -1,14 +1,23 @@
 package controler;
-import org.kohsuke.github.*;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.Attributes;
 
 
 public class GithubBridge
 {
-	GitHub gitHub;
 	DownloadListener downloadListener;
 
 	public GithubBridge(DownloadListener downloadListener)
@@ -16,13 +25,37 @@ public class GithubBridge
 		this.downloadListener = downloadListener;
 		//downloadListener.onDownloadFinished(null);
 
+
 		try
 		{
+			String jsonString = "[";
 
-			GHRepository repository = gitHub.getRepository("https://github.com/AndreasHennings/Perceptronix");
-			GHContent c = repository.getReadme();
-			String s = c.toString();
-			downloadListener.onDownloadFinished(s);
+			URL url = new URL("https://api.github.com/repos/AndreasHennings/Perceptronix");
+			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+			System.out.println(connection.getResponseCode());
+			if (connection.getResponseCode()==HttpsURLConnection.HTTP_OK)
+			{
+				InputStream is = connection.getInputStream();
+				BufferedReader br = new BufferedReader(new InputStreamReader(is));
+				String line;
+				while ((line=br.readLine())!=null)
+				{
+					jsonString+=line;
+
+				}
+				br.close();
+				is.close();
+			}
+
+
+
+			connection.disconnect();
+			jsonString+="]";
+			JSONArray jsonArray = new JSONArray(jsonString);
+			JSONObject jsonObject = jsonArray.getJSONObject(0);
+			String name = jsonObject.getString("name");
+			System.out.println(name);
+
 		}
 
 		catch (IOException e)
